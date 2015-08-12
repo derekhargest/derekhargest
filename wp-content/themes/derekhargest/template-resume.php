75e06cec7aa5a78e3bc0384773e7b2f85f583c1c
derekhargest <derekhargest@gmail.com>
2015-08-12 01:37:50 -0400

    portfolio

---
+++ wp-content/themes/derekhargest/template-resume.php
@@ -0,0 +1,21 @@
+<?php /* Template Name: Resume Template */ ?>
+
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+	<div class="container">
+		<div class="page-title">
+			<h1><?php the_title(); ?></h1>
+		</div>
+	</div>
+	<div class="page-content">
+		<div class="container resume">
+		</div>
+	</div>
+<?php endwhile; else : ?>
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+
+<?php get_footer(); ?>
